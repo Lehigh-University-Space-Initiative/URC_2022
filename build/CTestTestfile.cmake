181550fc3324f6b2f7605aac034d09359f65a211
Michael Baron <michael@ubuntu-linux--18.localdomain>
2022-11-13 13:22:21 -0500

    create ros workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/michael/URC_2022/src
+# Build directory: /home/michael/URC_2022/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/michael/URC_2022/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/michael/URC_2022/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/michael/URC_2022/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/michael/URC_2022/src:$ROS_PACKAGE_PATH"
