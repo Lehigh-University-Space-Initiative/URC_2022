ca055676df3536fc03cd49d2ab6ab83980c020d7
Michael Baron <mike.init246@gmail.com>
2023-03-27 18:55:45 -0400

    fix script

--- src/base_nodes/launch/localStartCMD.bash
+++ src/base_nodes/launch/localStartCMD.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source /opt/ros/melodic/setup.bash
-source /home/michael/URC_2022/devel_cb/setup.bash
+source ~/URC_2022/devel_cb/setup.bash
 
 export ROS_MASTER_URI=http://192.168.1.3:11311
 export ROS_IP=192.168.1.250
