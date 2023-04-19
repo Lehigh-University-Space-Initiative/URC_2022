7f1380c3c2ba90c47e0892c7d57263a1b1ec2fac
Tina <tip226@lehigh.edu>
2023-04-19 20:43:43 +0000

    added debug panel functionality

--- src/base_nodes/src/gui/panels/SoftwareDebugPanel.cpp
+++ src/base_nodes/src/gui/panels/SoftwareDebugPanel.cpp
@@ -1,7 +1,49 @@
 #include "SoftwareDebugPanel.h"
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <chrono>
 
 void SoftwareDebugPanel::drawBody()
 {
+    ImGui::Columns(6, "debugInfoColumns");
+    ImGui::Text("Date and Time");
+    ImGui::NextColumn();
+    ImGui::Text("Git Commit Hash");
+    ImGui::NextColumn();
+    ImGui::Text("Git Commit Message");
+    ImGui::NextColumn();
+    ImGui::Text("Git Branch");
+    ImGui::NextColumn();
+    ImGui::Text("Git User Name");
+    ImGui::NextColumn();
+    ImGui::Text("Git User Email");
+    ImGui::NextColumn();
+    ImGui::Separator();
+
+    for (const auto& line: debugInfoLines) {
+        std::istringstream iss(line);
+        std::string dateTime, commitHash, commitMessage, branch, userName, userEmail;
+        std::getline(iss, dateTime, ',');
+        std::getline(iss, commitHash, ',');
+        std::getline(iss, commitMessage, ',');
+        std::getline(iss, branch, ',');
+        std::getline(iss, userName, ',');
+        std::getline(iss, userEmail, ',');
+        ImGui::Text("%s", dateTime.c_str());
+        ImGui::NextColumn();
+        ImGui::Text("%s", commitHash.c_str());
+        ImGui::NextColumn();
+        ImGui::Text("%s", commitMessage.c_str());
+        ImGui::NextColumn();
+        ImGui::Text("%s", branch.c_str());
+        ImGui::NextColumn();
+        ImGui::Text("%s", userName.c_str());
+        ImGui::NextColumn();
+        ImGui::Text("%s", userEmail.c_str());
+        ImGui::NextColumn();
+    }
+    ImGui::Columns(1);
 }
 
 void SoftwareDebugPanel::setup()
@@ -10,4 +52,23 @@ void SoftwareDebugPanel::setup()
 
 void SoftwareDebugPanel::update()
 {
+    double currentTime = std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::system_clock::now().time_since_epoch()).count();
+    if (currentTime - lastUpdateTime > refreshInterval) {
+        readDebugInfoFile("/home/odroid/.ros/debug_info.txt");
+        lastUpdateTime = currentTime;
+    }
+}
+
+void SoftwareDebugPanel::readDebugInfoFile(const std::string& filePath)
+{
+    std::ifstream file(filePath);
+    if (file.is_open()) {
+        std::string line;
+        std::vector<std::string> lines;
+        while (std::getline(file, line)) {
+            lines.push_back(line);
+        }
+        debugInfoLines = std::move(lines);
+        file.close();
+    }
 }
--- src/base_nodes/src/gui/panels/SoftwareDebugPanel.h
+++ src/base_nodes/src/gui/panels/SoftwareDebugPanel.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "../Panel.h"
 #include "../GUI.h"
+#include <vector>
+#include <string>
 
 class SoftwareDebugPanel: public Panel {
 protected:
-
     virtual void drawBody() override;
-
-    
-
 public:
     using Panel::Panel;
 
     virtual void setup() override;
     virtual void update() override;
+
+private:
+    std::vector<std::string> debugInfoLines;
+    double lastUpdateTime;
+    const double refreshInterval = 0.5; // seconds
+
+    void readDebugInfoFile(const std::string& filePath);
 };
