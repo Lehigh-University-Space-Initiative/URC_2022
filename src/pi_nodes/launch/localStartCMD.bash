af490e49b6392937af088816bc6012b94ac5867c
Michael Baron <mike.init246@gmail.com>
2023-06-17 11:40:35 -0700

    progress made at end of year?

--- src/pi_nodes/launch/localStartCMD.bash
+++ src/pi_nodes/launch/localStartCMD.bash
@@ -9,10 +9,10 @@ export ROS_IP=192.168.1.5
 
 # Setup CAN
 
-echo lusi | sudo -S ip link set can0 up type can bitrate 1000000
-echo lusi | sudo -S ip link set can1 up type can bitrate 1000000
-echo lusi | sudo -S ifconfig can0 txqueuelen 65536
-echo lusi | sudo -S ifconfig can1 txqueuelen 65536
+# echo lusi | sudo -S ip link set can0 up type can bitrate 1000000
+# echo lusi | sudo -S ip link set can1 up type can bitrate 1000000
+# echo lusi | sudo -S ifconfig can0 txqueuelen 65536
+# echo lusi | sudo -S ifconfig can1 txqueuelen 65536
 
 # if file of this name exists on disk, don't auto start
 if [ -f "/home/pi/urc_deploy/src/pi_nodes/launch/no_auto_start" ] && [ "$1" != "-f" ]; then
